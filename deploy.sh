93f71e14a3e275a10ba644acf887885a66cd8508
NaorTheEnlightened <theenlightened02@gmail.com>
2022-12-16 09:25:44 +0200

    updates

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ cd dist
 
 git init
 git add -A
-git commit -m "New Deployment"
+#git commit -m "New Deployment"
+git commit -m $0
 git push -f git@github.com:NaorTheEnlightened/money-accelerator-replica.git master:gh-pages
 
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,10 +1,15 @@
 <template>
-$END$
+  <div>
+    <nav>
+      <h1> The Money Accelerator </h1>
+      <img src="../assets/infinite-money-logo.jpg" >
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
-name: "Main"
+  name: "Main"
 }
 </script>
 
