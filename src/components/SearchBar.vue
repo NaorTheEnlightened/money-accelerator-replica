7e3152bc4f624097c49c0a734c80acfb08ecc18f
NaorTheEnlightened <theenlightened02@gmail.com>
2022-12-25 16:06:56 +0200

    Added search bar functionality to filter results

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <input placeholder="Search" type="text" v-model="searchText" @input="$emit('filterSearch', searchText)">
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+      searchText: ''
+    }
+  },
+
+}
+</script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <TheNavbar></TheNavbar>
+    <TheNavbar />
     <section class="flex">
-      <Pictures></Pictures>
-      <Folder v-for="(module, i) in courseData" :module="module" :key="module.id" :index="i + 1"></Folder>
+      <Pictures />
+      <SearchBar @filterSearch="filterSearch" />
+      <Folder v-for="(module, i) in filteredData" :module="module" :key="module.id" :index="i + 1" />
     </section>
   </div>
 </template>
@@ -13,15 +14,33 @@ import TheNavbar from "@/components/TheNavbar";
 import Folder from "@/components/Folder";
 import jsonData from '../courseData/data.json'
 import Pictures from "@/components/Pictures";
+import SearchBar from "@/components/SearchBar";
 
 export default {
   name: "Main",
-  components: {Pictures, Folder, TheNavbar},
+  components: {SearchBar, Pictures, Folder, TheNavbar},
   data() {
     return {
       courseData: [...jsonData],
+      lastSearchText: ''
     }
-  }
+  },
+  methods: {
+    filterSearch(text) {
+      this.lastSearchText = text
+    }
+  },
+  computed: {
+    filteredData () {
+      const filteredData = JSON.parse(JSON.stringify(this.courseData))
+      return filteredData.filter(c => {
+        c.children = c.children.filter((file) => {
+          return file.file_name.includes(this.lastSearchText)
+        })
+        return !!c.children.length
+      })
+    }
+  },
 }
 </script>
 
